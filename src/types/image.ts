247d6a720f082aa4ae02b119d0e07bf6d1517ebf
Nikaple <nikaple1@gmail.com>
2025-07-24 15:31:49 +0800

    fix: types

--- src/types/image.ts
+++ src/types/image.ts
@@ -4,11 +4,11 @@ export interface ImageOptions {
   /** The URL of the image to be processed. */
   url: string;
   /** The [x, y] coordinates for the top-left corner of the image when compositing. */
-  position?: [number, number];
+  position: [number, number];
+  /** The desired width to resize the image to before compositing. */
+  width: number;
   /** An array defining the pixels to clip from the [top, right, bottom, left] edges. */
   clip?: [number, number, number, number];
-  /** The desired width to resize the image to before compositing. */
-  width?: number;
   /** The desired height to resize the image to. Aspect ratio is maintained based on width. */
   height?: number;
   /** The blend mode to use when compositing the image. */
